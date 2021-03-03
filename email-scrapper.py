b71c0daeea7583e58dc9e66a9e08527a01988d78
Filip911 <57196505+Filip911@users.noreply.github.com>
2021-03-03 14:54:09 -0500

    Update email-scrapper.py

--- email-scrapper.py
+++ email-scrapper.py
@@ -1,9 +1,13 @@
-from bs4 import Beautifulsoup
-import request
+from bs4 import BeautifulSoup
+import requests
 import requests.exceptions
 import urllib.parse
 from collections import deque
 import re
+#pip3 install requests
+#pip3 install bs4
+
+
 
 usr_url = str(input('[+]Enter User URL To Scan[+]'))
 urls = deque([user_url])
